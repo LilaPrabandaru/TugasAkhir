62a56bce5d057734d18df9cc12ebd542f6d035e6
Rafael Deandra <rafaeldeandra60@gmail.com>
2025-02-25 16:34:24 +0700

    Add Sidebar to User View

--- frontend/src/layout/DefaultUserLayout.js
+++ frontend/src/layout/DefaultUserLayout.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import { AppFooter, AppHeader } from '../components/index';
+import { AppFooter, AppHeader, AppSidebar} from '../components/index';
 import UserDashboard from '../views/user/UserDashboard';
 
 const DefaultUserLayout = () => {
   return (
-    <div className="wrapper d-flex flex-column min-vh-100">
-      <AppHeader />
-      <div className="body flex-grow-1">
-        <Routes>
-          {/* Redirect otomatis dari /user ke /user/dashboard */}
-          <Route index element={<Navigate to="dashboard" />} />
-          <Route path="dashboard" element={<UserDashboard />} />
-        </Routes>
-      </div>
-      <AppFooter />
+    <div>
+        <AppSidebar />
+        <div className="wrapper d-flex flex-column min-vh-100">
+        <AppHeader />
+        <div className="body flex-grow-1">
+            <Routes>
+            {/* Redirect otomatis dari /user ke /user/dashboard */}
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<UserDashboard />} />
+            </Routes>
+        </div>
+        <AppFooter />
+        </div>
     </div>
   );
 };
