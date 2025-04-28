5c7bcacaee69a6e4903292357231b6da02659b80
lila <672021017@student.uksw.edu>
2025-04-28 19:01:13 +0700

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'http://localhost:5000'
+  API_URL: 'https://tugas-akhir-rzk2-ovtmbbss7-lila-prabandarus-projects.vercel.app',
   // API_URL: 'https://be.iffan.online'
   // API_URL: 'https://effective-fishstick-v7jwrqpq4rx2j67-5000.app.github.dev/'
   // API_URL: 'https://05f1-139-255-192-106.ngrok-free.app'
